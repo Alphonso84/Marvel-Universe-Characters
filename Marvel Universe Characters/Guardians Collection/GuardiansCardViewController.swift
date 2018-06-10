9ca408e5253064c7011ab487305f0b24057bcb1f
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-06-10 06:58:34 -0700

    Building on another view for guardians of the galaxy

---
+++ Marvel Universe Characters/Guardians Collection/GuardiansCardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GuardiansCardViewController.swift
+//  Marvel Universe Characters
+//
+//  Created by user on 6/10/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GuardiansCardViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var guardianHeroes = [Hero]()
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
